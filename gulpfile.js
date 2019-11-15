134504792f134f7dfb32d175092e5848afbebf61
Cho-Ching Lu <chochinlu@gmail.com>
2019-11-16 00:12:50 +0800

    add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-const { src, dest } = require("gulp");
+const { src, dest, series } = require("gulp");
 const mustache = require("gulp-mustache");
 const htmlmin = require("gulp-htmlmin");
 const inlineCss = require("gulp-inline-css");
+const del = require("del");
 
-exports.default = function() {
+function clean() {
+  return del(["./dist"]);
+}
+
+function htmlcss() {
   return src("./templates/*.mustache")
     .pipe(mustache({ user: "Park" }, { extension: ".html" }))
     .pipe(inlineCss({ removeHtmlSelectors: true }))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest("./dist"));
-};
+}
+
+const build = series(clean, htmlcss);
+
+exports.clean = clean;
+exports.htmlcss = htmlcss;
+exports.default = build;
