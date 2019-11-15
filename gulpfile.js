8bf2e6a3d8acf6ef7de95bc74fc88ee49c111d81
Cho-Ching Lu <chochinlu@gmail.com>
2019-11-15 23:51:22 +0800

    add inline-css and minify-html support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest } = require("gulp");
 const mustache = require("gulp-mustache");
+const htmlmin = require("gulp-htmlmin");
+const inlineCss = require("gulp-inline-css");
 
 exports.default = function() {
   return src("./templates/*.mustache")
     .pipe(mustache({ user: "Park" }, { extension: ".html" }))
+    .pipe(inlineCss({ removeHtmlSelectors: true }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest("./dist"));
 };
