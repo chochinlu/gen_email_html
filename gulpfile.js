3768b0e500e69cb7122b1a7b01ba9387caa636a6
Cho-Ching Lu <chochinlu@gmail.com>
2019-11-16 09:59:26 +0800

    change source architecture

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,35 @@ const htmlmin = require("gulp-htmlmin");
 const inlineCss = require("gulp-inline-css");
 const del = require("del");
 
+const issue = process.argv[4];
+const sourceDir = `./src/${issue}`;
+const sourceTemplate = `${sourceDir}/*.mustache`;
+const jsonEn = `${sourceDir}/en.json`;
+const target = `./dist/${issue}`;
+
+function hello(done) {
+  console.log("hello issue: ", issue);
+  console.log("source:　", sourceTemplate);
+  console.log('en json: ', jsonEn)
+  console.log("target:　", target);
+  done();
+}
+
 function clean() {
   return del(["./dist"]);
 }
 
 function htmlcss() {
-  return src("./templates/*.mustache")
-    .pipe(mustache({ user: "Park" }, { extension: ".html" }))
+  return src(sourceTemplate)
+    .pipe(mustache(jsonEn, { extension: ".html" }))
     .pipe(inlineCss({ removeHtmlSelectors: true }))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest("./dist"));
+    .pipe(dest(target));
 }
 
-const build = series(clean, htmlcss);
+const build = series(hello, clean, htmlcss);
 
+exports.hello = hello;
 exports.clean = clean;
 exports.htmlcss = htmlcss;
 exports.default = build;
