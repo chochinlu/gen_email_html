ba686afbcf749e392100857354b18a6e90c882b1
Park.Lu <park.lu@nogle.com>
2020-11-11 16:10:33 +0800

    update logic

--- genConfig.js
+++ genConfig.js
@@ -1,9 +1,12 @@
 const readline = require('readline')
 const fs = require('fs')
 
+const SRC = './src'
+
 const TARGET_TEMPLATE = 'index.mustache'
 
-const emailTemplate = `<!doctype html>
+// default email template if user doesn't give any default template
+const EMAIL_TEMPLATE = `<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,77 +21,106 @@ const emailTemplate = `<!doctype html>
 </html>
 `
 
-console.log('gen basic configs')
+const DEFAULT_CONFIG = {
+  inject: {
+    frontString: [],
+    endString: [],
+  },
+}
 
-const SRC = './src'
+const genConfigStr = (langArray) => {
+  return JSON.stringify(
+    {
+      ...DEFAULT_CONFIG,
+      langs: langArray,
+    },
+    null,
+    2
+  )
+}
 
+const DEFAULT_LANG = {
+  user: 'world',
+}
+
+const genLangObjStr = (langStr) => {
+  return JSON.stringify(
+    {
+      ...DEFAULT_LANG,
+      lang: langStr,
+    },
+    null,
+    2
+  )
+}
+
+const langJsonPath = (folderPath, langStr) => {
+  return `${folderPath}/${langStr}.json`
+}
+
+console.log('[START] gen basic files....')
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 })
 
 rl.question(
-  'Input your folder name? (project name or issue number): ',
-  folderName => {
-    console.log('your folder name is ', folderName)
-
+  'Please input your folder name (project name or issue number): ',
+  (folderName) => {
     // check the folder if exist or empty
     const folderPath = `${SRC}/${folderName}`
     if (fs.existsSync(folderPath)) {
-      console.log('Folder has existed, you should choose another one.')
+      console.error(
+        `[ERROR] Folder: ${folderName} has existed, you should choose another one.`
+      )
       rl.close()
     } else {
       // create folder
-      console.log('create folder: ', folderName)
-      fs.mkdir(folderPath, err => {
+      console.log('[CREATE] folder ', folderName)
+      fs.mkdir(folderPath, (err) => {
         if (err) {
-          console.log('Create folder failed: ', err)
+          console.error(`[ERROR] Create folder ${folderName} failed: ${err}`)
           rl.close()
         }
-        console.log('Folder created.')
+        console.log(`[INFO] Folder ${folderName}  created.`)
 
         rl.question(
-          'What default language do you want add? (default: en)',
-          defaultLang => {
-            const lang = defaultLang === '' ? 'en' : defaultLang
-            console.log('your default lang is ', defaultLang)
+          'Use the common templates? If yes, input your common template folder name: ',
+          (commonTemplateFolderName) => {
+            if (!commonTemplateFolderName) {
+              // use the default generate rule
+              rl.question(
+                'What default language do you want add? (default: en)',
+                (defaultLang) => {
+                  const lang = defaultLang === '' ? 'en' : defaultLang
+                  console.log('your default lang is ', defaultLang)
 
-            const jsonPath = `${folderPath}/${lang}.json`
-            const defaultLangJsonObj = { lang, user: 'world' }
+                  const jsonPath = `${folderPath}/${lang}.json`
+                  const configPath = `${folderPath}/config.json`
 
-            const configPath = `${folderPath}/config.json`
-            const defaultConfigObj = {
-              langs: [lang],
-              inject: {
-                frontString: [],
-                endString: []
-              }
-            }
+                  const templatePath = `${folderPath}/${TARGET_TEMPLATE}`
 
-            const templatePath = `${folderPath}/${TARGET_TEMPLATE}`
+                  try {
+                    fs.writeFileSync(jsonPath, genLangObjStr(lang), 'utf8')
+                    console.log(`${jsonPath} created`)
 
-            try {
-              fs.writeFileSync(
-                jsonPath,
-                JSON.stringify(defaultLangJsonObj, null, 2),
-                'utf8'
+                    fs.writeFileSync(configPath, genConfigStr([lang]), 'utf8')
+                    console.log(`${configPath} created`)
+
+                    fs.writeFileSync(templatePath, EMAIL_TEMPLATE, 'utf8')
+                    console.log(`${templatePath} created`)
+                  } catch (err) {
+                    console.log(err)
+                  }
+
+                  rl.close()
+                }
               )
-              console.log(`${jsonPath} created`)
-
-              fs.writeFileSync(
-                configPath,
-                JSON.stringify(defaultConfigObj, null, 2),
-                'utf8'
-              )
-              console.log(`${configPath} created`)
-
-              fs.writeFileSync(templatePath, emailTemplate, 'utf8')
-              console.log(`${templatePath} created`)
-            } catch (err) {
-              console.log(err)
+            } else {
+              // use the common template folder to generate the target template
+              // check if this folder exists
+              console.log('Common Template exists')
             }
-
-            rl.close()
           }
         )
       })
